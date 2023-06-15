ad3ec2ada36dce0dcca527b1f8e49a0ec8852997
Yurij Orlik <81624225+Louis301@users.noreply.github.com>
2023-06-15 22:53:23 +0300

    Add files via upload

---
+++ Timer/Timer.cpp
@@ -0,0 +1,32 @@
+﻿// example.cpp: определяет точку входа для консольного приложения.
+//
+
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <conio.h>
+#include <process.h>
+
+DWORD WINAPI MyThread(LPVOID lpParam)
+{
+    puts("Help me");
+    return 0;
+}
+
+int _tmain()
+{
+    DWORD dwThreadId;
+    HANDLE hThread = CreateThread(
+        NULL,              // default security attributes
+        0,                 // use default stack size  
+        MyThread,          // thread function 
+        NULL,             // argument to thread function 
+        0,                 // use default creation flags 
+        &dwThreadId);   // returns the thread identifier 
+    _getch();
+
+
+    return 0;
+}
+
+ 
